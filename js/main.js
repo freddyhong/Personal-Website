eeb97d57167de5fab4d6c991d0ec2e688d75cde7
wongi hong <wghong22@gmail.com>
2023-11-19 02:45:54 -0500

    contact section added

--- js/main.js
+++ js/main.js
@@ -168,7 +168,7 @@
   // Click outside of offcanvass
   var mobileMenuOutsideClick = function () {
     $(document).click(function (e) {
-      var container = $("#colorlib-aside, .js-colorlib-nav-toggle");
+      var container = $("#main-aside, .js-colorlib-nav-toggle");
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($("body").hasClass("offcanvas")) {
           $("body").removeClass("offcanvas");
